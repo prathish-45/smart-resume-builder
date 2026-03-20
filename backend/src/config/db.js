99d1f5899254948c69ceb795aa59f928e617e89a
prathish-45 <prathishsit2427@ksrce.ac.in>
2026-03-20 21:33:36 +0530

    feat: Implement initial MongoDB connection and environment variable setup.

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/smart-resume-builder');
+    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://prathish_db_user:12345@cluster0.gsqnt4p.mongodb.net/smart-resume-builder?appName=Cluster0');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);

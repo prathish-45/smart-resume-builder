c90f4f0d5baf622aba7623d3928845c7577916b4
prathish-45 <prathishsit2427@ksrce.ac.in>
2026-03-21 13:04:47 +0530

    feat: Initialize main application structure with routing, context providers, and analytics.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";

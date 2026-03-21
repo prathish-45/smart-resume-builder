a20976d4c8a957b7bb436f166dcfddc443eb6e61
prathish-45 <prathishsit2427@ksrce.ac.in>
2026-03-21 12:53:31 +0530

    feat: Set up core application routing, state management, and Vercel analytics/speed insights.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -23,6 +24,7 @@ const App = () => (
         <Toaster />
         <Sonner />
         <Analytics />
+        <SpeedInsights />
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Navigate to="/login" replace />} />
